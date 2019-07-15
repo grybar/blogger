f1ec1c98fe08db907b3c9a1a69342061effab811
Garrett <gmrybarczyk@gmail.com>
2019-07-15 10:36:29 -0600

    Sorcery authentication complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_12_160912) do
+ActiveRecord::Schema.define(version: 2019_07_13_204704) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -45,6 +45,16 @@ ActiveRecord::Schema.define(version: 2019_07_12_160912) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "email", null: false
+    t.string "crypted_password"
+    t.string "salt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
